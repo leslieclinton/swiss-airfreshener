64f728e6b22daa84068e25cbfe9821f50c5c57a4
leslieclinton <leslieclinton20@gmail.com>
2021-08-26 02:50:30 +0100

    Mobile Header

--- src/components/headers/mobileHeader/MobileHeader.js
+++ src/components/headers/mobileHeader/MobileHeader.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import './MobileHeader.css'
+import './MobileHeader.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import SlickSlider from "react-slick";
+
 
 function MobileHeader() {
+
+    const settings = {
+        dots: false,
+        fade: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        pauseOnHover: true,
+        cssEase: "linear",
+        arrows: false
+      };
+
     return (
         <>
+        <SlickSlider {...settings}>
             <div className="m__header m__slide-1">
+               
+                    <div className="m__header-content">
+                    <div className="m__header-content-container">
+                        <h3>Swiss Flower</h3>
+                        <h1>Scent Pad</h1>
+                        <p>Simple ingredients with stellar results.  in-house garment care line is tried.Simple ingredients with stellar </p>
+
+                        <div className="m__header-btn">
+                            Explore
+                        </div>
+
+                        </div>
+                </div>
+            </div>
+            <div className="m__header m__slide-2">
                
                     <div className="m__header-content">
                     <div className="m__header-content-container">
@@ -19,6 +53,7 @@ function MobileHeader() {
                         </div>
                 </div>
             </div>
+            </SlickSlider>
         </>
     )
 }
