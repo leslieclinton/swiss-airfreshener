2339e7f1a098d5e24dee3da7fecd69557ed6f000
leslieclinton <leslieclinton20@gmail.com>
2021-08-26 14:00:28 +0100

    Products preview

--- src/components/productPreview/ProductPreview.js
+++ src/components/productPreview/ProductPreview.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation } from 'swiper';  
 
 import './ProductPreview.css';
 
-import 'swiper/swiper.scss'
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+// import 'swiper/components/navigation/pagination.scss';
+// import 'swiper/components/navigation/scrollbar.scss';
 
 
 
 
 function ProductPreview() {
+    SwiperCore.use([Navigation]);
+
+
+    const [resize,  setResize] = useState('desktop');
+
+    useEffect(()=>{
+
+
+    }, [])
+
+
     return (
 
         <div className="productpreview">
@@ -18,10 +33,26 @@ function ProductPreview() {
 
 
             <Swiper
-            spaceBetween={50}
+            breakpoints={{
+                // when window width is >= 640px
+                300: {
+                  width: 300,
+                  slidesPerView: 1.2,
+                  navigation :"false"
+                },
+                // when window width is >= 768px
+                748: {
+                  width: 740,
+                  slidesPerView: 2,
+                },
+              }}
+            modules={[Navigation]}
+            spaceBetween={20}
             slidesPerView={4}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
+            navigation
+            pagination={{ clickable: true }}
             >
             <SwiperSlide>
                 <Link to='/'>
@@ -34,36 +65,36 @@ function ProductPreview() {
             </SwiperSlide>
             <SwiperSlide>
                 <Link to='/'>
-                    <div className="productpreview-images productpreview-images-1">
+                    <div className="productpreview-images productpreview-images-2">
                         <div className="productpreview-products">
-                            <span>Liquid</span>
+                            <span>Regular & Refil</span>
                         </div>
                     </div>
                 </Link> 
             </SwiperSlide>
             <SwiperSlide>
                 <Link to='/'>
-                    <div className="productpreview-images productpreview-images-1">
+                    <div className="productpreview-images productpreview-images-3">
                         <div className="productpreview-products">
-                            <span>Liquid</span>
+                            <span> Card</span>
                         </div>
                     </div>
                 </Link> 
             </SwiperSlide>
             <SwiperSlide>
                 <Link to='/'>
-                    <div className="productpreview-images productpreview-images-1">
+                    <div className="productpreview-images productpreview-images-4">
                         <div className="productpreview-products">
-                            <span>Liquid</span>
+                            <span>Scent Pad</span>
                         </div>
                     </div>
                 </Link> 
             </SwiperSlide>
             <SwiperSlide>
                 <Link to='/'>
-                    <div className="productpreview-images productpreview-images-1">
+                    <div className="productpreview-images productpreview-images-5">
                         <div className="productpreview-products">
-                            <span>Liquid</span>
+                            <span>Gel</span>
                         </div>
                     </div>
                 </Link> 
